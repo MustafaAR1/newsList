4bd3eb437423475cfae6e308a16a08b44251c4b1
MUSTAFAAR1 <mustafaahmedraza@gmail.com>
2021-11-03 14:34:39 +0500

    piot done

---
+++ lib/src/articles.dart
@@ -0,0 +1,119 @@
+class Articles {
+  final String author;
+  final String title;
+  final String url;
+  Articles({required this.author, required this.title, required this.url});
+}
+
+final articles = [
+  Articles(
+    author: '港電台-港聞',
+    title: '斯克：我們跟 Hertz 並沒有簽約，特斯拉也不缺訂單',
+    url:
+        "https://img.technews.tw/wp-content/uploads/2021/11/03150953/%E6%88%AA%E5%9C%96-2021-11-03-%E4%B8%8B%E5%8D%883.09.34-624x367.png",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 3 ',
+    title: "לראשונה: טסלה Model 3 הבסיסית עולה יותר מ-200 אלף שקלים",
+    url:
+        "https://www.businesstoday.in/latest/world/story/elon-musks-cryptic-post-goes-viral-in-china-amid-conflict-with-world-food-programme-311214-2021-11-03",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+  Articles(
+    author: 'News number 2',
+    title:
+        't IAM Auto IP conference, calls for patent licensing meet exhortations to hold out: more automotive SEP litigation may just be inevitable',
+    url:
+        "https://crops.giga.de/2b/58/32/49fc20682ab04cb1425be14da2_YyAyNjk3eDE0MDkrMTYrNDY0AnJlIDEyMDAgNjI3AzI4NGM2YTQxNTFh.jpg",
+  ),
+];
